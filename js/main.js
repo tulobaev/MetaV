72dea1225ffba9be5bcb7f0cc34240abc59dd62f
tulobaev <talgattulobaev519>
2025-01-06 17:45:21 +0600

    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// document.addEventListener("mouseover", function (e) {
+//   const cursor = document.querySelector(".cursor-follow");
+//   cursor.style.left = `${e.pageX}px`;
+//   cursor.style.top = `${e.pageY}px`;
+//   // Изменение цвета курсора в зависимости от положения
+//   const red = Math.min(255, e.pageX % 255);
+//   const green = Math.min(255, e.pageY % 255);
+// });
+
 let menu = document.querySelector(".burger");
 let modal = document.querySelector(".modal_menu");
 let exit_menu = document.querySelector(".exit_menu");
@@ -7,13 +16,4 @@ menu.addEventListener("click", () => {
 });
 exit_menu.addEventListener("click", () => {
   modal.style.display = "none";
-});
-
-document.addEventListener("mousemove", function (e) {
-  const cursor = document.querySelector(".cursor-follow");
-  cursor.style.left = `${e.pageX}px`;
-  cursor.style.top = `${e.pageY}px`;
-  // Изменение цвета курсора в зависимости от положения
-  const red = Math.min(255, e.pageX % 255);
-  const green = Math.min(255, e.pageY % 255);
 });
